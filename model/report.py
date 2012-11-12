29a29af45ed6131bb4c1044c47b0b4b027d8e0b3
yupeng <yupbank@gmail.com>
2012-11-12 17:12:09 +0800

    ok

--- model/report.py
+++ model/report.py
@@ -10,18 +10,34 @@ Created on
 '''
 from group import user_id_by_group_id
 from blog import get_author_id_by_user_id, Blog, Comment, get_user_id_by_author_id, get_user_id_by_blog_id, get_author_id_by_blog_id
+from user import User
 
 def reports_by_group_id(group_id):
     in_edge = []
+    cache = {}
     for user_id in user_id_by_group_id(group_id):
         blog_id = Blog.get(user_id=user_id).id
         author_id = get_author_id_by_user_id(user_id)
         comments = Comment.where(author_id=author_id)
         counts = count_in (comments)
+        if cache.get(user_id):
+            user = cache.get(user_id)
+        else:
+            user = User.get(user_id)
+            cache[user_id] = user
+        user = User.get(user_id)
         for i, j in counts.iteritems():
             if user_id <> i:
+                if i in cache:
+                    target = cache[i]
+                else:
+                    target = User.get(i)
+                    cache[i] = target
+
                 edge = {'source': user_id,
+                        'source_name': user.name,
                         'target': i,
+                        'target_name': target.name,
                         'counts': j,
                         }
                 in_edge.append(edge)
